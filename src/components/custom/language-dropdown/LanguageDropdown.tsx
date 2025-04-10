7caa78614045292e7a50950d33e9b0785488e1f1
akrap19 <matija.akrap@gmail.com>
2025-04-10 13:41:02 +0200

    pushing finished languages

--- src/components/custom/language-dropdown/LanguageDropdown.tsx
+++ src/components/custom/language-dropdown/LanguageDropdown.tsx
@@ -40,15 +40,15 @@ export const LanguageDropdown = () => {
 		{ id: 'bg', name: 'Languages.bg' },
 		{ id: 'de', name: 'Languages.de' },
 		{ id: 'dk', name: 'Languages.dk' },
-		{ id: 'ee', name: 'Languages.ee' },
-		{ id: 'slo', name: 'Languages.slo' },
-		{ id: 'esp', name: 'Languages.esp' },
-		{ id: 'fi', name: 'Languages.fi' },
-		{ id: 'is', name: 'Languages.is' },
-		{ id: 'lt', name: 'Languages.lt' },
-		{ id: 'lv', name: 'Languages.lv' },
-		{ id: 'no', name: 'Languages.no' },
-		{ id: 'pl', name: 'Languages.pl' }
+		// { id: 'ee', name: 'Languages.ee' },
+		// { id: 'slo', name: 'Languages.slo' },
+		{ id: 'esp', name: 'Languages.esp' }
+		// { id: 'fi', name: 'Languages.fi' },
+		// { id: 'is', name: 'Languages.is' },
+		// { id: 'lt', name: 'Languages.lt' },
+		// { id: 'lv', name: 'Languages.lv' },
+		// { id: 'no', name: 'Languages.no' },
+		// { id: 'pl', name: 'Languages.pl' }
 	]
 
 	const handleDropDownOpening = (e: React.MouseEvent<HTMLButtonElement>) => {
